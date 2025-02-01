f4bdcec384ab690ef50e6795a9ea10755ddc0d1c
ChatGIS <ChatGIS@outlook.com>
2025-02-01 22:23:38 +0800

    v1.3.0 add wkt2DegreesArray

--- one/data-type.ts
+++ one/data-type.ts
@@ -1,3 +1,12 @@
+export function wkt2DegreesArray(wkt: string) {
+  /* if (wkt.includes('POINT') || wkt.includes('Point') || wkt.includes('point')) {
+    return wkt.split('(')[1].split(')')[0].split(' ').map(item => Number(item))
+  } else if (wkt.includes('LINESTRING') || wkt.includes('LineString') || wkt.includes('linestring')) {
+    return wkt.split('(')[1].split(')')[0].split(',').map(item => item.split(' ').map(item => Number(item)))
+  } else */ if (wkt.includes('POLYGON') || wkt.includes('Polygon') || wkt.includes('polygon')) {
+    return wkt.split('((')[1].split('))')[0].replaceAll(' ', ',').split(',').map(item => Number(item))
+  }
+}
 export function wkt2CoorArray(wkt: string) {
     if (wkt.includes('POINT') || wkt.includes('Point') || wkt.includes('point')) {
       return wkt.split('(')[1].split(')')[0].split(' ').map(item => Number(item))
